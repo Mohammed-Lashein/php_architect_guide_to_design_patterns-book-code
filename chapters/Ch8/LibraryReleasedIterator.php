7d5a29f871b9e27f899b9521ec8fa5741cd7b6b1
Mohammed-Lashein <mohammedlashein2700@gmail.com>
2025-09-04 14:47:33 +0300

    refactor(LibraryReleasedIterator): use sortedItems property instead of sorting the collection in place to follow immutability principle

--- chapters/Ch8/LibraryReleasedIterator.php
+++ chapters/Ch8/LibraryReleasedIterator.php
@@ -6,15 +6,17 @@ class LibraryReleasedIterator extends VariantIterator {
   private array $sortedItems = [];
   public function __construct(array $collection) {
     parent::__construct($collection); // sets the value of $this->collection
-    // somewhow sort the collection 
-    usort($this->collection, fn($a, $b) => $a->getYear() - $b->getYear());
+    // get a copy of $collection
+    $this->sortedItems = $this->collection;
+
+    usort($this->sortedItems, fn($a, $b) => $a->getYear() - $b->getYear());
   }
 
   public function next(): Lendable|bool {
     if($this->isFirstCall) {
       $this->isFirstCall = false;
-      return current($this->collection);
+      return current($this->sortedItems);
     }
-    return next($this->collection);
+    return next($this->sortedItems);
   }
 }
